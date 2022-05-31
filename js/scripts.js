c1b14073e097d3ceb6ea253d402d984b2be35b0e
asiantempura <imjerry99@gmail.com>
2022-05-31 03:45:27 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
